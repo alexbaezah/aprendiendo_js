3c1a2daf857073d494855579665a59c9d5ea5a74
alexbaezah <alcebaeza@gmail.com>
2020-09-21 01:20:10 -0300

    app_2.js creado

---
+++ app_2.js
@@ -0,0 +1,36 @@
+const suma = (ns) => {
+    let acumulado = 0;
+    for (i = 0; i < ns.length; i++) {
+        acumulado += ns[i]
+    }
+    return acumulado
+}
+
+const numeros = [1, 2, 3, 4, 5];
+
+//multiplicar * 2
+
+// mult = numeros.map(x => x * 2);
+// console.log(mult);
+
+// tuplas
+
+// tuplas = numeros.map(x => [x, x]);
+// console.log(tuplas)
+
+// edad promedio 
+
+
+const mascotas = [
+    { nombre: "lynda", edad: 4, raza: "perro"},
+    { nombre: "chanchito feliz", edad: 3, raza: "perro"},
+    { nombre: "pulga", edad: 10, raza: "perro"},
+    { nombre: "lunita", edad: 2, raza: "gato"}
+];
+
+edades = mascotas.map( x => x.edad); 
+console.log(edades);
+
+total= suma(edades);
+console.log(total);
+console.log(total / edades.length)
